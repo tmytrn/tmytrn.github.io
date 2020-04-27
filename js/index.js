935d4db3bee55b17f0f4742c0c1f142b3835dd19
Tommy Tran <elitemasterdragonslayer123@gmail.com>
2020-04-26 20:09:02 -0700

    first wave of style changes

---
+++ js/index.js
@@ -0,0 +1,9 @@
+window.onload= function() {Particles.init({
+    selector: '.background',
+    color: '#33ff33',
+    connectParticles: true,
+		maxParticles: 60,
+		speed: 0.4,
+    sizeVariations: 6,
+  });
+};
