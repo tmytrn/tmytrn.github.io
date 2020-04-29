8120c23e4154f10397ead98eb922317736f4b98d
Tommy Tran <elitemasterdragonslayer123@gmail.com>
2020-04-28 18:33:20 -0700

    particle changes

--- js/index.js
+++ js/index.js
@@ -1,10 +1,45 @@
-window.onload= function() { Particles.init({
+window.onload = function () {
+  Particles.init({
     selector: '.particle',
-    color: '#20215b',
+    color: '#ffffff',
     connectParticles: true,
-		maxParticles: 45,
-		speed: 0.3,
+    speed: 0.3,
     sizeVariations: 6,
+    responsive: [
+      {
+        breakpoint:
+          768
+        ,
+        options: {
+          maxParticles:
+            50
+          ,
+          connectParticles:
+            true
+        }
+      }, {
+        breakpoint:
+          425
+        ,
+        options: {
+          maxParticles:
+            40
+          ,
+          connectParticles:
+            true
+        }
+      }, {
+        breakpoint:
+          320
+        ,
+        options: {
+          maxParticles:
+            25
+
+          // disables particles.js
+        }
+      }
+    ]
   });
 };
 
