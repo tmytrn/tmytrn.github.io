9a4dad15b9f623c4b9a68a255f237ea531f17750
Tommy Tran <elitemasterdragonslayer123@gmail.com>
2020-06-29 18:28:52 -0700

    Updates

---
+++ precache-manifest.867e119b21f5e957ac93904ce396c7f0.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4741c7414f601e8154db56c4103282b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cba6dc456bd631d120f",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ae0c39eba15798a04ec2",
+    "url": "/static/css/main.73107b56.chunk.css"
+  },
+  {
+    "revision": "4cba6dc456bd631d120f",
+    "url": "/static/js/2.d1232c78.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d1232c78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae0c39eba15798a04ec2",
+    "url": "/static/js/main.32f8116f.chunk.js"
+  },
+  {
+    "revision": "f83efad6a0e5fd348c60",
+    "url": "/static/js/runtime-main.dc340ee8.js"
+  },
+  {
+    "revision": "68b9f2b3d0eb407a833a2043de3c388b",
+    "url": "/static/media/AirbnbJournals.68b9f2b3.png"
+  },
+  {
+    "revision": "dfd69e99234a1a945e66772845841bc2",
+    "url": "/static/media/AirbnbJournals_Hero.dfd69e99.png"
+  },
+  {
+    "revision": "f1a0c267f30bc458ae15663205262631",
+    "url": "/static/media/CB.f1a0c267.png"
+  },
+  {
+    "revision": "7990f6a78a3792ccdbbd8e0a76c31d0c",
+    "url": "/static/media/Current_Home.7990f6a7.png"
+  },
+  {
+    "revision": "ef40fe33a96e846153e076a029543f32",
+    "url": "/static/media/DLS.ef40fe33.png"
+  },
+  {
+    "revision": "3c6c04189d2ef25118c1909a0c36311b",
+    "url": "/static/media/Effort_Impact.3c6c0418.png"
+  },
+  {
+    "revision": "a8a6109a2ca7aef44b5782c3b3e1c375",
+    "url": "/static/media/FilmReview.a8a6109a.png"
+  },
+  {
+    "revision": "b3660a4b77544d970dd4d93fdb297b0f",
+    "url": "/static/media/HomePage@2x.b3660a4b.png"
+  },
+  {
+    "revision": "91c8787fbd4fa30e888a7963b8c3dfca",
+    "url": "/static/media/Journal_1@2x.91c8787f.png"
+  },
+  {
+    "revision": "dda82e500b392efd6696b7ced4ee75a8",
+    "url": "/static/media/Journal_Memento@2x.dda82e50.png"
+  },
+  {
+    "revision": "da15b37486ed9ab173dc160e3f1fd588",
+    "url": "/static/media/Journals_Home@2x.da15b374.png"
+  },
+  {
+    "revision": "31845050206538aaf3efd53a7386bbb1",
+    "url": "/static/media/Luxe.31845050.png"
+  },
+  {
+    "revision": "c64dff8aa3146cbbdb49b2260d4c2ad7",
+    "url": "/static/media/Online_Experiences.c64dff8a.png"
+  },
+  {
+    "revision": "206df8b9d12b4679ee3cfcf9c1b2b387",
+    "url": "/static/media/PA_Logo_wide.206df8b9.jpg"
+  },
+  {
+    "revision": "5c69f3318e3486d2c73069beb937c5f3",
+    "url": "/static/media/Plus.5c69f331.png"
+  },
+  {
+    "revision": "b06a6a33b51860f7c4e1339c6da92626",
+    "url": "/static/media/TeamAdobo_AirbnbCreativeJam_Add_To_Journal.b06a6a33.mp4"
+  },
+  {
+    "revision": "2fafb0a9e4d98a314e848eb347bf6694",
+    "url": "/static/media/TeamAdobo_AirbnbCreativeJam_Submission.2fafb0a9.mp4"
+  },
+  {
+    "revision": "10f2ac39fbc376af003574fce8ae1ca1",
+    "url": "/static/media/User_Journey.10f2ac39.png"
+  },
+  {
+    "revision": "0a891c3d4df935ba9eda793205888204",
+    "url": "/static/media/tommy-tran-@tmytrn-website-portrait-2020-by-benjamin-siordia.0a891c3d.jpg"
+  }
+]);
